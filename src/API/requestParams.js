121617458c3e9c09294348b5bd35ae85c083507d
Vadim Matsko <mvadim3101@gmail.com>
2021-05-11 19:29:05 +0300

    Full HW-04

---
+++ src/API/requestParams.js
@@ -0,0 +1,6 @@
+const requestParams = {
+  API_KEY: "40090b959646918577b40b0314a59aec",
+  BASE_URL: "https://api.themoviedb.org/3",
+};
+
+export default requestParams;
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const routes = {
+  home: "/",
+  movies: "/movies",
+  movie: "/movie",
+  cast: "/credits",
+  reviews: "/reviews",
+};
+
+export default routes;
